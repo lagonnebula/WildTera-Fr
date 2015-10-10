87564393f25d9634c70721af74b83bb053cd560b
lagonnebula <laginnebula2@gmail.com>
2015-10-10 11:28:21 +0200

    Added payments file

---
+++ page/payments/fr-FR/index.js
@@ -0,0 +1,18 @@
+module.exports = {
+    virtualItems: {
+        from: 'support@playwildterra.com',
+        fromName: 'Wild Terra',
+        subject: 'Early Access Bundle',
+        youBuy: 'You have purchased: ',
+        yourKeys: 'Your key for accessing to the game: ',
+        templateID: 'fd23b7e6-6212-4519-897f-79a653184f0f'
+    },
+    generateAndSend: {
+        from: 'support@playwildterra.com',
+        fromName: 'Wild Terra',
+        subject: 'Early Access Key',
+        youGot: 'You have got access key: ',
+        yourKeys: 'Your access key: ',
+        templateID: '924cfd4f-db38-4cb0-b787-9ceff7c88c3d'
+    }
+};
